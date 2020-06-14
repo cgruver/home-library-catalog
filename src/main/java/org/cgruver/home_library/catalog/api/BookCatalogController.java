43ba60721a6046b79889bcf8a3c46a63ffd00e03
Charro Gruver <charro_gruver@yahoo.com>
2020-06-14 12:15:27 -0400

    test initial service integration

--- src/main/java/org/cgruver/home_library/catalog/api/BookCatalogController.java
+++ src/main/java/org/cgruver/home_library/catalog/api/BookCatalogController.java
@@ -1,11 +1,16 @@
 package org.cgruver.home_library.catalog.api;
 
+import javax.inject.Inject;
+
 import org.cgruver.home_library.catalog.client.BookCatalogException;
 import org.cgruver.home_library.catalog.client.api.BookCatalogApi;
 import org.cgruver.home_library.catalog.client.dto.BookInfoDTO;
+import org.cgruver.home_library.catalog.service.BookCatalogService;
 
 public class BookCatalogController implements BookCatalogApi {
 
+    @Inject
+    private BookCatalogService bookCatalogService;
     @Override
     public BookInfoDTO saveBookInfo(BookInfoDTO bookInfo) throws BookCatalogException {
         // TODO Auto-generated method stub
@@ -14,8 +19,7 @@ public class BookCatalogController implements BookCatalogApi {
 
     @Override
     public BookInfoDTO getBookInfo(String isbn) throws BookCatalogException {
-        // TODO Auto-generated method stub
-        return null;
+        return bookCatalogService.getBookInfoByIsbn(isbn);
     }
     
 }
